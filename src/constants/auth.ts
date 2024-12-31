eac0737386a59727638fac848c53222faaec4c2d
PushkarOP <PushkarOP@users.noreply.github.com>
2024-12-31 07:28:11 +0000

    Prettified Code!

--- src/constants/auth.ts
+++ src/constants/auth.ts
@@ -1,5 +1,6 @@
 export const officialAPIEndpoint = 'https://api.openai.com/v1/chat/completions';
-const customAPIEndpoint = 'https://api.nexusmind.tech/nexus/v3/chat/completions';
+const customAPIEndpoint =
+  'https://api.nexusmind.tech/nexus/v3/chat/completions';
 export const defaultAPIEndpoint = customAPIEndpoint;
 
 export const availableEndpoints = [customAPIEndpoint];
