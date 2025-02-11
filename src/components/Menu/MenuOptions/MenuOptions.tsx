863947599a0903f72c834d9d23f5b54cf9fa0fa1
pushkarop <pushkarsingh4349@gmail.com>
2025-02-11 08:53:53 +0530

    update MenuOptions.tsx

--- src/components/Menu/MenuOptions/MenuOptions.tsx
+++ src/components/Menu/MenuOptions/MenuOptions.tsx
@@ -6,6 +6,7 @@ const MenuOptions = () => {
   return (
     <>
       <div className={`max-h-full py-1 overflow-hidden transition-all`}>
+        <SettingsMenu />
         <a
           className='flex py-2 px-2 items-center gap-3 rounded-md hover:bg-custom-white/20 transition-colors duration-200 text-custom-white cursor-pointer text-sm'
           href='https://www.nexusmind.tech/partners'
@@ -14,7 +15,6 @@ const MenuOptions = () => {
         >
           <ApiIcon className='w-4 h-4' /> Free API
         </a>
-        <SettingsMenu />
       </div>
     </>
   );
